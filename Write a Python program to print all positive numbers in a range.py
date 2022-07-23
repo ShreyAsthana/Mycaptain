63d5080fe08fc45a784aa2129d21a40e940ebaa1
Shrey Asthana <67062923+ShreyAsthana@users.noreply.github.com>
2022-07-23 17:05:38 +0530

    Write a Python program to print all positive numbers in a range.

---
+++ Write a Python program to print all positive numbers in a range.py
@@ -0,0 +1,13 @@
+list1=[12, -7, 5, 64, -14]
+for ele in list1:
+    if ele<0:
+        pass
+    else:
+        print(ele)
+
+list2=[2, 14, -95, 3]
+for ele in list2:
+    if ele<0:
+        pass
+    else:
+        print(ele)
